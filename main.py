f2be2f7c9557439620900cf56263915b9d989431
godmilklv <godmilklv@gmail.com>
2022-07-11 14:42:47 -0700

    Initial Commit

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ import os
 # App initializations
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL1", "sqlite:///blog.db")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL", "sqlite:///blog.db")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 ckeditor = CKEditor(app)
